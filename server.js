75ab71d6e36d1964a666416a6daa23c9454beb65
Shivam singh <shivam2003sy@gmail.com>
2024-02-24 12:41:17 +0530

    cors

--- server.js
+++ server.js
@@ -19,12 +19,7 @@ const io = socketIo(server , {
   }
 });
 
-app.use(cors(
-  {
-    origin: 'http://localhost:3000',
-    credentials: true
-  }
-)); 
+app.use(cors()); 
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
