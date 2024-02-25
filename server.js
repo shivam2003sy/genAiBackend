98a2beb5a56b537dac55aee58357a19d8fdabd82
Shivam singh <shivam2003sy@gmail.com>
2024-02-25 22:13:33 +0530

    cors

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const { addEmailToSocketId } = require("./socket/helper");
 require('dotenv').config();
 connectDb();
 const app = express();
+app.use(cors()); 
 const server = http.createServer(app);
 const io = socketIo(server , {
   cors: {
@@ -19,7 +20,7 @@ const io = socketIo(server , {
   }
 });
 
-app.use(cors()); 
+
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
